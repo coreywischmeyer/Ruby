f73fbc4ce9fb0480e52a886211f621d9397f64d3
Corey Wischmeyer <corey@Abulafia.(none)>
2014-04-06 13:26:41 -0500

    Ruby count nucleotides

---
+++ Rosalind/count_nt/count_nt.rb
@@ -0,0 +1,7 @@
+string = ARGV[0].chomp
+a_count = string.count("A")
+g_count = string.count("G")
+c_count = string.count("C")
+t_count =string.count("T")
+
+print(a_count, g_count, c_count, t_count);
